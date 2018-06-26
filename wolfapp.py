823929c4f1951445a361a837fe90472cf3ae93f3
Dennis Afanasev <dennis.afa@gmail.com>
2018-06-26 14:07:17 -0400

    update

--- wolfapp.py
+++ wolfapp.py
@@ -103,6 +103,7 @@ def run():
     # wolf359: 201885041
     # josh's star: 201205469
     # ran: 206208968
+    # test
 
     w359 = KeplerTargetPixelFile.from_archive(201885041, cadence='short')
     lc359 = w359.to_lightcurve(aperture_mask=w359.pipeline_mask)
