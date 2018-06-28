da60cee24fdb8116d3a1ca0a5a9406609b691df2
Dennis Afanasev <dennis.afa@gmail.com>
2018-06-28 13:24:19 -0400

    work

--- wolfapp.py
+++ wolfapp.py
@@ -14,7 +14,6 @@ from george import kernels
 class strPlot:
 
     def neg_ln_like(self, p):
-        '''return'''
         self.gp.set_parameter_vector(p)
         return -self.gp.log_likelihood(self.flux)
 
@@ -23,18 +22,16 @@ class strPlot:
         return -self.gp.grad_log_likelihood(self.flux)
 
 
-    def findMaxFlux(self, flux):
+    def findflaremax(self, flux):
         return max(flux)
 
-    def findFluxTime(self, peakFlux, flux, time):
+    def findflaretime(self, flarepeak, flux, time): # retrieves the time of the flare
         tof = time
-        i = 0
-        for x in flux:
-            if (x == peakFlux):
+        for i, flare in enumerate(flux):
+            if flare == flarepeak:
                 return tof[i]
-            i += 1
 
-    def getModel(self, p, data):
+    def getmodel(self, p, data): # computes the model of the flares using appaloosa's aflare1 function
         time, y, nflares = data
         p = np.exp(p)
         model = np.zeros_like([time])
@@ -45,10 +42,10 @@ class strPlot:
 
     def ng_ln_like(self, p, data):
         _, y, _ = data
-        model = self.getModel(p, data)
+        model = self.getmodel(p, data)
         return np.sum((model - y) ** 2)
 
-    def __init__(self, star, range1, range2):
+    def __init__(self, star, range1, range2): # cleans the list of flux, normalizes to 0
         sap = star.remove_outliers()
         self.flux = sap.flux[range1:range2]
         self.time = sap.time[range1:range2]
@@ -56,18 +53,17 @@ class strPlot:
         self.flux = [number / scipy.std(self.flux) for number in self.flux]
         self.time = self.time[np.isfinite(self.flux)]
         self.flux = asarray(self.flux)
-        self.flux = self.flux[np.isfinite(self.flux)]  # removes NaN values
-        print(self.flux)
+        self.flux = self.flux[np.isfinite(self.flux)]
 
 
-    def guesspeaks(self, sliceNum):
+    def guesspeaks(self, sliceNum): # gathers the peaks in the set of data, then returns a list of flare times, peaks, and fwhm
         self.detflares = fd.flaredetect(self.flux, sliceNum)
-        self.nflares = np.shape(self.detflares)[0] # used to get the shape of the array, number of flares is the shape
+        self.nflares = np.shape(self.detflares)[0]
         self.params = np.zeros([self.nflares, 3])
         for i, flareVal in enumerate(self.detflares):
-            self.peakFlare = flareVal
-            self.peakTime = self.findFluxTime(self.peakFlare, self.flux, self.time)
-            p = [self.peakTime, 0.004, self.peakFlare]
+            self.flarepeak = flareVal
+            self.flaretime = self.findflaretime(self.flarepeak, self.flux, self.time)
+            p = [self.flaretime, 0.004, self.flarepeak]
             self.params[i, :] = p
         return np.log(self.params)
 
@@ -99,42 +95,77 @@ class strPlot:
 
 
 
-def run():
+def load():
+    w359 = KeplerTargetPixelFile.from_archive(201885041, cadence='short')
+    lc359 = w359.to_lightcurve(aperture_mask=w359.pipeline_mask)
+    
+def call(lc359, flux):
     # wolf359: 201885041
     # josh's star: 201205469
     # ran: 206208968
-    # merging from masterclone
-    # test2
-
     w359 = KeplerTargetPixelFile.from_archive(201885041, cadence='short')
     lc359 = w359.to_lightcurve(aperture_mask=w359.pipeline_mask)
-    fluxwolf = lc359.flux
+    track = 0
+    steps = 100
+    length = 1000
+    for slice in range(0, 100, steps):
+
+        flare = strPlot(lc359, slice, slice + steps)
+        guessparams = flare.guesspeaks(track)
+        notempty = checkzero(flare.detflares)
+        if notempty:
+            georgemodel = flare.computegeorge()
+            bounds = flare.setbounds(guessparams)
+
+            fitparams = flare.fit(guessparams, bounds)
+            model = flare.getmodel(fitparams, [flare.time, flare.flux, flare.nflares]) + georgemodel
+            plotflares(flare, model, track)
+
+            slice += steps
+            track += 1
+            print("Success at range {}".format(slice))
+        else:
+            print("No flares in slice {}".format(slice))
+            slice+=steps
+            track+=1
+
+def checkzero(l):
+    if len(l) == 0:
+        return False
+    else:
+        return True
+
+def plotflares(flare, model, track):
+    for it, flux in enumerate(flare.detflares):
+        pl.plot(flare.params[it, 0], flux, marker='x', markersize=4, color="black")
 
 
-    i = 0
-    x = 0
-    range = 100
-    length = len(fluxwolf)
-    while i < length - range:
-        flare = strPlot(lc359, i, i + range)
-        #georgemodel = flare.computegeorge()
-        #print(georgemodel)
-        guessparams = flare.guesspeaks(x)
-        bounds = flare.setbounds(guessparams)
-        if len(flare.detflares) == 0:
-            i += range
-            continue
-        fitparams = flare.fit(guessparams, bounds)
-        model = flare.getModel(fitparams, [flare.time, flare.flux, flare.nflares])
-        for it, flux in enumerate(flare.detflares):
-            pl.plot(flare.params[it, 0], flux, marker='x', markersize=4, color="black")
-        pl.plot(flare.time, model.flatten(), '--r')
-        pl.plot(flare.time, flare.flux, color='Grey', lw=0.5)
-        pl.xlabel('Time - BYJD')
-        pl.ylabel('Flux - Normalized to 0')
-        savepath = os.path.join('/Users/Dennis/Desktop/plotswolf/test', 'wolf' + str(x) + '.png')
-        pl.savefig(savepath)
-        pl.clf()
-        i += range
-        x += 1
-        print("Success at range " + str(i))
+    pl.plot(flare.time, model.flatten(), '--r')
+    pl.plot(flare.time, flare.flux, color='Grey', lw=0.5)
+    pl.xlabel('Time - BYJD')
+    pl.ylabel('Flux - Normalized to 0')
+    pl.show()
+    pl.clf()
+
+    while flare.guesspeaks(track, model.flatten())
+    pl.plot(flare.time, (flare.flux-model.flatten()), color = 'Black', label = 'Subtracted flares')
+    pl.plot(flare.time, flare.flux, color = 'Grey', linestyle = '--', label = 'Original')
+    pl.legend(loc = 'upper-left')
+    pl.xlabel('Time - BYJD')
+    pl.ylabel('Flux - Normalized to 0')
+    pl.show()
+
+
+    savepath = os.path.join('/Users/Dennis/Desktop/plotswolf/test', 'wolf' + str(track) + '.png')
+    pl.savefig(savepath)
+    pl.clf()
+
+def cleandata(star, range1, range2):
+    sap =  star.remove_outliers()
+    flux = sap.flux[range1:range2]
+    time = sap.time[range1:range2]
+    flux = (flux / np.median(flux)) - 1
+    flux = [number / scipy.std(flux) for number in flux]
+    time = time[np.isfinite(flux)]
+    flux = asarray(flux)
+    flux = flux[np.isfinite(flux)]
